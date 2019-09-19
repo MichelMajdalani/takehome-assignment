9eee8f461908dea568cc41ea7dbadeb2a1212cdd
Michel Majdalani <michel.majdalani@mail.mcgill.ca>
2019-09-18 23:06:55 -0400

    Added part 3 vue

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,5 +1,10 @@
 <template>
-  <p>I'm a Counter!</p>
+  <div>
+    <p>I'm a Counter!</p>
+    <p>Current count value: {{count}}</p>
+    <button v-on:click="count +=1">Increment</button>
+    <button v-on:click="count -=1">Decrement</button>
+  </div>
 </template>
 
 <script>
