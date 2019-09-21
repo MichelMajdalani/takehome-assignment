78ad19277c361d5cd9f21044d217f67554e98a21
Michel Majdalani <michel.majdalani@mail.mcgill.ca>
2019-09-21 19:48:17 -0400

    Prop fixed

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <p>I'm a Counter!</p>
-    <p>Current count value: {{count}}</p>
+    <p>Current count value: {{currentCount}}</p>
     <button v-on:click="increment">Increment</button>
     <button v-on:click="decrement">Decrement</button>
   </div>
@@ -15,12 +15,17 @@ export default {
       type: Number
     }
   },
+  data() {
+    return {
+      currentCount: this.count
+    }
+  },
   methods: {
     increment: function() {
-      this.count += 1
+      this.currentCount += 1;
     },
     decrement: function() {
-      this.count -= 1
+      this.currentCount -= 1;
     }
   }
 };
